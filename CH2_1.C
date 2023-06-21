794e84f1724ff73c51011d3b11fdfa101bd4f9b5
imparasparabiya <137284876+imparasparabiya@users.noreply.github.com>
2023-06-21 16:58:20 +0530

    Add files via upload

---
+++ CH2_1.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+  clrscr();
+  printf("- - - - - - - - - \n");
+  printf("|\t\t|\n");
+  printf("|\t\t|\n");
+  printf("|PARAS PARABIYA |\n");
+  printf("|\t\t|\n");
+  printf("|\t\t|\n");
+  printf("- - - - - - - - - \n");
+  getch();
+
+}
---
+++ CH2_3.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+  clrscr();
+  printf("~ ~ ~ ~ ~ ~ ~ ~ ~\n");
+  printf("*\t\t*\n");
+  printf("P\t\t*\n");
+  printf("R\t\t*\n");
+  printf("I\t\t*\n");
+  printf("N\t\t*\n");
+  printf("C\t\t*\n");
+  printf("E\t\t*\n");
+  printf("*\t\t*\n");
+  printf("~ ~ ~ ~ ~ ~ ~ ~ ~\n");
+
+
+  getch();
+
+}
